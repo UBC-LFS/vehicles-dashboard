0886b2dbcbf12b2511174805dd20d2f50b1de7b1
Edmund Seow <edmund.seow@ubc.ca>
2025-02-19 17:37:29 -0800

    Fixed typo

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'includes/database.php';
-include 'includes/queries.php'
-include 'includes/login.php'
+include 'includes/queries.php';
+include 'includes/login.php';
 ?>
 
 <!DOCTYPE html>
