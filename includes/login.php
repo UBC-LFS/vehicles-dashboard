26b813c1f464c75f16d16f21bfbde39d1596620a
Edmund Seow <edmund.seow@ubc.ca>
2025-02-21 11:08:00 -0800

    changed admin test to a function

--- includes/login.php
+++ includes/login.php
@@ -9,8 +9,12 @@ $as->requireAuth();
 
 $attributes = $as->getAttributes();
 
-if (!(in_array($attributes['urn:mace:dir:attribute-def:cwlLoginName'][0], $auth['saml']['admin']['urn:mace:dir:attribute-def:cwlLoginName']))) {
-        print("<h1>Vehicle Administrators only</h1>");
-        exit();
+function is_admin() {
+    if (in_array($attributes['urn:mace:dir:attribute-def:cwlLoginName'][0], $auth['saml']['admin']['urn:mace:dir:attribute-def:cwlLoginName'])) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
 ?>
